36041fcfb0ccaca9fe3fac7485b5383e99a15160
Ghita <18383@ecam.be>
2024-05-27 23:47:12 +0200

    reorganise the pjt and delete corrupted images

--- components/navigation/TabBarIcon.tsx
+++ components/navigation/TabBarIcon.tsx
@@ -1,1 +1,7 @@
-// You can explore the built-in icon families and icons on the web at https://icons.expo.fyi/import Ionicons from '@expo/vector-icons/Ionicons';import { type IconProps } from '@expo/vector-icons/build/createIconSet';import { type ComponentProps } from 'react';export function TabBarIcon({ style, ...rest }: IconProps<ComponentProps<typeof Ionicons>['name']>) {  return <Ionicons size={28} style={[{ marginBottom: -3 }, style]} {...rest} />;}
+import Ionicons from '@expo/vector-icons/Ionicons';
+import { IconProps } from '@expo/vector-icons/build/createIconSet';
+import { ComponentProps } from 'react';
+
+export default function TabBarIcon({ style, ...rest }: IconProps<ComponentProps<typeof Ionicons>['name']>) {
+  return <Ionicons size={28} style={[{ marginBottom: -3 }, style]} {...rest} />;
+}
